a5dc2a25639b98cc137ee1b4e9879ac7cc90d0d7
alendill <alendill@hotmail.com>
2020-01-09 00:10:35 +0800

    ftp协议解析

--- src/main/java/orion/transfer/core/MainApp.java
+++ src/main/java/orion/transfer/core/MainApp.java
@@ -1,11 +1,18 @@
 package orion.transfer.core;
 
+import orion.transfer.core.common.DownloadLinkParser;
+import orion.transfer.core.exception.DownloadLinkException;
+import orion.transfer.core.info.DownloadLinkInfo;
 import orion.transfer.core.util.FtpUtil;
 
 public class MainApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DownloadLinkException {
         System.out.println("迅雷就是个鸡掰！");
         String url = "ftp://ygdy8:ygdy8@yg39.dydytt.net:3004/阳光电影www.ygdy8.com.哆啦A梦：大雄的月球探险记.BD.1080p.国粤日三语中字.mkv";
-        FtpUtil.download(url);
+//        FtpUtil.download(url);
+
+        DownloadLinkParser instance = DownloadLinkParser.getInstance();
+        DownloadLinkInfo downloadLinkInfo = instance.protocolSeparate(url);
+        System.out.println(downloadLinkInfo.toString());
     }
 }
--- src/main/java/orion/transfer/core/common/DownloadLinkParser.java
+++ src/main/java/orion/transfer/core/common/DownloadLinkParser.java
@@ -1,9 +1,10 @@
-package orion.transfer.core.util;
+package orion.transfer.core.common;
 
 import org.apache.commons.lang3.StringUtils;
 import orion.transfer.core.exception.DownloadLinkException;
 import orion.transfer.core.info.DownloadLinkInfo;
 import orion.transfer.core.info.FtpLinkInfo;
+import orion.transfer.core.info.HttpLinkInfo;
 
 public class DownloadLinkParser {
     private static final String PREFIX_FTP = "ftp";
@@ -29,17 +30,19 @@ public class DownloadLinkParser {
         return instance;
     }
 
-    private DownloadLinkInfo protocolSeparate(String url) throws DownloadLinkException {
+    public DownloadLinkInfo protocolSeparate(String url) throws DownloadLinkException {
         if (StringUtils.isEmpty(url)) {
             throw new DownloadLinkException("Download url can not be null/empty!");
         }
         DownloadLinkInfo downloadLinkInfo = null;
-        switch (url.split("://")[0]) {
+        String[] split = url.split("://");
+        switch (split[0]) {
             case PREFIX_FTP:
-                downloadLinkInfo = parseFtp(url);
+                downloadLinkInfo = parseFtp(split[1]);
                 break;
             case PREFIX_HTTP:
-
+                downloadLinkInfo = parseHttp(split[1]);
+                break;
         }
         return downloadLinkInfo;
     }
@@ -48,15 +51,30 @@ public class DownloadLinkParser {
     /**
      * 从ftp开头的url中解析组装成FtpLinkInfo
      *
-     * @param url url
+     * @param arg url
      * @return FtpLinkInfo
      */
-    private FtpLinkInfo parseFtp(String url) {
-        if (true) {
+    private FtpLinkInfo parseFtp(String arg) {
+        FtpLinkInfo info = new FtpLinkInfo();
+        String[] split = arg.split("@");
+        mkUser(info, split[0].split(":"));
+        mkUri(info, split[1].substring(0, split[1].indexOf("/")).split(":"));
+        info.setProtocol(PREFIX_FTP);
+        return info;
+    }
 
+    private void mkUser(FtpLinkInfo ftpLinkInfo, String[] userInfo) {
+        ftpLinkInfo.setUsername(userInfo[0]);
+        ftpLinkInfo.setPassword(userInfo[1]);
+    }
 
-        }
-        return new FtpLinkInfo();
+    private void mkUri(FtpLinkInfo ftpLinkInfo, String[] userInfo) {
+        ftpLinkInfo.setHost(userInfo[0]);
+        ftpLinkInfo.setPort(Integer.parseInt(userInfo[1]));
+    }
+
+    private HttpLinkInfo parseHttp(String arg) {
+        return new HttpLinkInfo();
     }
 
 }
