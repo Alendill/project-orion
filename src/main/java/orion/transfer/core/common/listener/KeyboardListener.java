97e7d0f9a459572d37d5a61ffc54f840ee53e447
alendill <alendill@hotmail.com>
2020-01-16 23:14:54 +0800

    update

---
+++ src/main/java/orion/transfer/core/common/listener/KeyboardListener.java
@@ -0,0 +1,7 @@
+package orion.transfer.core.common.listener;
+
+/**
+ * 键盘事件监听器
+ */
+public class KeyboardListener {
+}
---
+++ src/main/java/orion/transfer/core/common/trigger/OperationTrigger.java
@@ -0,0 +1,7 @@
+package orion.transfer.core.common.trigger;
+
+/**
+ * 操作触发器
+ */
+public class OperationTrigger {
+}
